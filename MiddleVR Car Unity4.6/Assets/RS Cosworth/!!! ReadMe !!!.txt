074110e4ccaa9c369362c6d1b875a9d9b67e0dbf
villj2 <villj2@bfh.ch>
2015-04-13 20:33:00 +0200

    Init Commit

---
+++ MiddleVR Car Unity4.6/Assets/RS Cosworth/!!! ReadMe !!!.txt
@@ -0,0 +1,33 @@
+----------------------------------------------------------------------
+3D Model of Car - Ford RS Cosworth
+
+Visit My Site - http://3d-auto-club.blogspot.com/
+
+3D Cars Models, Reference of Cars, Blueprints & OTHER Stuff...
+----------------------------------------------------------------------
+
+<-- Archives of News / Архивы Новостей: -->
+
+3D Models of Cars:
+In folder are 3D models of various cars.
+http://sharingmatrix.com/folder/73
+
+
+Wallpapers of Cars:
+In folder are Wallpapers of various cars.
+http://sharingmatrix.com/folder/74
+
+
+Blueprints of Cars:
+In folder are Blueprints of various cars.
+http://sharingmatrix.com/folder/97
+
+
+Other Wallpapers:
+In folder there are Different Wllpapers which do not concern to Cars.
+http://sharingmatrix.com/folder/86
+
+
+Other 3D models:
+In folder there are Different 3D Models, and also Models for Poser.
+http://sharingmatrix.com/folder/161
---
+++ MiddleVR Car Unity4.6/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour : MonoBehaviour {
+
+	public WheelCollider wheelFL;
+	public WheelCollider wheelFR;
+	public float maxTorque = 50;
+	public GUIText guiSpeed;
+	public Texture2D guiSpeedDisplay;
+	public Texture2D guiSpeedPointer;
+
+	private float currentSpeedKMH = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.centerOfMass = new Vector3 (0, 0, 0);
+	}
+
+	// Update is called once per frame constanc time per frame
+	void FixedUpdate () {
+		wheelFL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+		wheelFR.motorTorque = wheelFL.motorTorque;
+		wheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
+		wheelFR.steerAngle = wheelFL.steerAngle;
+
+		//guiSpeed.text = wheelFL.rpm.ToString("0");
+		currentSpeedKMH = (rigidbody.velocity.magnitude * 3.6f);
+		guiSpeed.text = currentSpeedKMH.ToString("0");
+	}
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect (0, 0, 140, 140), guiSpeedDisplay);
+
+		float angle = Mathf.Abs(currentSpeedKMH) + 40;
+
+		GUIUtility.RotateAroundPivot (angle, new Vector2 (70, 70));
+		GUI.DrawTexture (new Rect (0, 0, 140, 140), guiSpeedPointer, ScaleMode.StretchToFill);
+	}
+}
